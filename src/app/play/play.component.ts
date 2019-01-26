91228558b761dfe22bfd84ab3cf53915078b32a5
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2019-01-26 20:49:15 +0000

    Start linking playing functionality

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-play',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayComponent implements OnInit {
 
-  constructor() { }
+  private id: number;
+
+  constructor(private route: ActivatedRoute, private authService: AuthenticationService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(data => {
+        this.id = +data['id'];
+        this.authService.getGame(this.id).subscribe( game => console.log(game));
+      });
   }
 
 }
