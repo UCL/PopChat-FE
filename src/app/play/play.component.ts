2d6f121344814e9f0fc7563f51716e5d968cbd72
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2019-01-29 17:13:54 +0000

    Embed videos in the game page

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
+import { map, switchMap } from 'rxjs/operators';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+interface Game {
+  url: string;
+}
 
 @Component({
   selector: 'app-play',
@@ -9,14 +15,24 @@ import { AuthenticationService } from '../authentication.service';
 })
 export class PlayComponent implements OnInit {
 
-  private id: number;
+  public id: number;
+  public game: Game;
 
-  constructor(private route: ActivatedRoute, private authService: AuthenticationService) { }
+  constructor(private route: ActivatedRoute, private authService: AuthenticationService, private sanitizer: DomSanitizer) { }
+
+  public getUrl(): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.game.url + '?&autoplay=1');
+  }
 
   ngOnInit() {
-    this.route.params.subscribe(data => {
-        this.id = +data['id'];
-        this.authService.getGame(this.id).subscribe( game => console.log(game));
+    this.route.params.pipe(
+      map(data => +data['id']),
+      switchMap( id => {
+        return this.authService.getGame(id);
+      })
+      ).subscribe( game => {
+        this.game = game;
+        console.log(game);
       });
   }
 
