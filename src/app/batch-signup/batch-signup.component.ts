371015cb3b848945c9e59ae1b4afb297155ec979
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2019-01-31 14:29:35 +0000

    Fix whitespace

--- src/app/batch-signup/batch-signup.component.ts
+++ src/app/batch-signup/batch-signup.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 
 interface PopUser {
-   username: string;
-   password: string;
+  username: string;
+  password: string;
 }
 
 @Component({
@@ -19,36 +19,35 @@ export class BatchSignupComponent implements OnInit {
 
   constructor(private authService: AuthenticationService) {
     this.success = false;
-    this.accounts  = [];
-   }
+    this.accounts = [];
+  }
 
   ngOnInit() {
   }
 
   public submit() {
-      this.success = false;
-      this.errorMessage = undefined;
-      const username = (<HTMLInputElement> document.getElementById('username-prefix')).value;
-      const number = (<HTMLInputElement> document.getElementById('number')).value;
-      this.authService.batchCreateAccounts(username, +number).subscribe(data => {
-        this.success = true;
-        console.log(data);
-        this.accounts = data;
-      }, error => {
-        console.log(error);
-        if (error.error !== undefined) {
-          if (error.error.message !== undefined) {
-            this.errorMessage = error.error.message;
-          } else if (error.error.error_description !== undefined) {
-            this.errorMessage = error.error.error_description;
-          } else if (error.error.error !== undefined) {
-            this.errorMessage = error.error.error;
-          } else {
-            this.errorMessage = 'An unknown error has occurred';
-          }
+    this.success = false;
+    this.errorMessage = undefined;
+    const username = (<HTMLInputElement>document.getElementById('username-prefix')).value;
+    const number = (<HTMLInputElement>document.getElementById('number')).value;
+    this.authService.batchCreateAccounts(username, +number).subscribe(data => {
+      this.success = true;
+      console.log(data);
+      this.accounts = data;
+    }, error => {
+      console.log(error);
+      if (error.error !== undefined) {
+        if (error.error.message !== undefined) {
+          this.errorMessage = error.error.message;
+        } else if (error.error.error_description !== undefined) {
+          this.errorMessage = error.error.error_description;
+        } else if (error.error.error !== undefined) {
+          this.errorMessage = error.error.error;
+        } else {
+          this.errorMessage = 'An unknown error has occurred';
         }
       }
-        );
-
+    }
+    );
   }
 }
