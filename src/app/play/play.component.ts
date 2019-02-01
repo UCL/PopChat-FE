cb06a7e4a7547bc6cc29cb80b786d919fb4bae77
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2019-02-01 17:13:10 +0000

    Add UI elements for playing the game (but not saving the data)

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -6,6 +6,19 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 interface Game {
   url: string;
+  endTime: number;
+  startTime: number;
+  questions: Array<Question>;
+}
+
+interface Question {
+  questionText: string;
+  answers: Array<Answer>;
+}
+
+interface Answer {
+  value: string;
+  correct: boolean;
 }
 
 @Component({
@@ -17,11 +30,31 @@ export class PlayComponent implements OnInit {
 
   public id: number;
   public game: Game;
+  public currentIndex: number;
 
-  constructor(private route: ActivatedRoute, private authService: AuthenticationService, private sanitizer: DomSanitizer) { }
+  constructor(private route: ActivatedRoute, private authService: AuthenticationService, private sanitizer: DomSanitizer) {
+    this.currentIndex = 0;
+   }
 
-  public getUrl(): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(this.game.url + '?&autoplay=1');
+  public getUrl(): string {
+    return this.game.url + '?start=' + this.game.startTime + '&end=' + this.game.endTime + '&autoplay=1&controls=0';
+  }
+
+  public submitAnswer(event: Event, answer: Answer): boolean {
+    event.preventDefault();
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    event.srcElement.classList.add(answer.correct ? 'right' : 'wrong');
+    console.log(answer.correct ? 'yay' : ':(');
+    setTimeout(() => {
+        event.srcElement.classList.remove(answer.correct ? 'right' : 'wrong');
+        if (answer.correct) {
+          if (this.currentIndex < this.game.questions.length) {
+            this.currentIndex += 1;
+          }
+        }
+    }, 800);
+    return false;
   }
 
   ngOnInit() {
@@ -33,6 +66,7 @@ export class PlayComponent implements OnInit {
       ).subscribe( game => {
         this.game = game;
         console.log(game);
+        (<HTMLIFrameElement>window.document.getElementById('video')).src = this.getUrl();
       });
   }
 
